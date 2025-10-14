892691659ce1dd237dfd02ae41c4a0d59a060003
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-10-15 00:52:32 +0530

    auth

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -8,9 +8,22 @@ app.use(cors());
 
 app.post("/project", async(req, res) => {
     const { prompt } = req.body;
+    const userId = req.userId;
     const description = prompt.split("\n")[0];
     const project = await primsaClient.project.create({
-        data: { description },
+        data: { description, userId },
     });
     res.json({ projectId: project.id });
+});
+
+app.get("/projects", async(req, res) => {
+    const userId = req.userId;
+    const project = await primsaClient.project.findUnique({
+        where: { userId },
+    });
+    res.json(project);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
