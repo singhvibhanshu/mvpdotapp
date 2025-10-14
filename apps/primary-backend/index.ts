1bbd9bb3074d03bf530e60e17ee1d80d28ff998c
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-10-15 00:26:40 +0530

    created the project endpoint

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/create-project", async(req, res) => {
-
-} )
+app.post("/project", async(req, res) => {
+    const { prompt } = req.body;
+    const description = prompt.split("\n")[0];
+    const project = await primsaClient.project.create({
+        data: { description },
+    });
+    res.json({ projectId: project.id });
+});
