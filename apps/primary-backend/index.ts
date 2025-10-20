bf56120e44f270c6e5d0f0814d9e8d2d94277fe9
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-10-20 06:49:42 +0530

    middlewares implementation done and basic auth too

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,12 +1,13 @@
 import { primsaClient } from "db/client";
 import express from "express";
 import cors from "cors";
+import { authMiddleware } from "./middleware";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/project", async (req, res) => {
+app.post("/project", authMiddleware, async (req, res) => {
     const { prompt } = req.body;
     const userId = req.userId;
     const description = prompt.split("\n")[0];
@@ -16,9 +17,9 @@ app.post("/project", async (req, res) => {
     res.json({ projectId: project.id });
 });
 
-app.get("/projects", async (req, res) => {
+app.get("/projects", authMiddleware, async (req, res) => {
     const userId = req.userId;
-    const project = await primsaClient.project.findUnique({
+    const project = await primsaClient.project.findFirst({
         where: { userId },
     });
     res.json(project);
--- apps/primary-backend/middleware.ts
+++ apps/primary-backend/middleware.ts
@@ -1,17 +1,27 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { use } from "react";
 
 export function authMiddleware(req:Request, res:Response, next:NextFunction) {
     const token = req.headers.authorization;
     if(!token) {
-        return res.status(401).json({ error: "Unauthorized" });
+        res.status(401).json({ error: "Unauthorized" });
+        return;
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_PUBLIC_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_PUBLIC_KEY!, { algorithms: ["RS256"] });
     if(!decoded) {
-        return res.status(401).json({ message: "Unauthorized" });
+        res.status(401).json({ message: "Unauthorized" });
+        return;
     }
 
-    req.userId = decoded.sub;
+    const userId = (decoded as any).payload.sub;
+
+    if(!userId) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+
+    req.userId = userId;
     next();
 }
